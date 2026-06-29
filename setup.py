ec447285dc100e9e23f7e943de7ba48b99c983a5
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-06-29 11:33:23 +0530

    add all requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements()->List[str]:
+    requirements_list:List[str]=[]
+    return  requirements_list
+
+setup(
+    name="Langchain",
+    version="0.0.1",
+    author="Shravan Kumar Pandey",
+    author_email="shravankumarpandey825412@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+
+)
