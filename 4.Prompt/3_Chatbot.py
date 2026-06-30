8a8737701c52080925de34aca9b70eaedad51a78
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-06-30 13:52:30 +0530

    ChatBot

---
+++ 4.Prompt/3_Chatbot.py
@@ -0,0 +1,17 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+model=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+chat_history = []
+
+while True:
+    user_input=input("User: ")
+    chat_history.append(user_input)
+    if user_input=="exit":
+        break
+    response=model.invoke(chat_history)
+    chat_history.append(response.content)
+    print("Bot: ", response.content)
+
+print("\nChat History:", chat_history)
