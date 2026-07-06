6ab60bc80567c098f12371e5d651055f9ea9359e
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-06 20:22:31 +0530

    text structure based text splitter

--- 9.RAG/2.Text_Splitter/2_text_structure_based_text_splitter.py
+++ 9.RAG/2.Text_Splitter/2_text_structure_based_text_splitter.py
@@ -14,3 +14,8 @@ splitter=RecursiveCharacterTextSplitter(
 )
 result=splitter.split_text(text)
 print(result)
+
+# output
+"""
+['Space exploration is the physical investigation of outer space by uncrewed robotic space probes and', 'through human spaceflight.', 'While the observation of objects in space, known as astronomy, predates reliable recorded history,', 'it was the development of large and relatively efficient rockets during the mid-twentieth century', 'that allowed physical space exploration to become a reality. Common rationales for exploring space', 'include advancing scientific research, national prestige, uniting different nations, ensuring the', 'future survival of humanity, and developing military and strategic advantages against other', 'countries.']
+"""
