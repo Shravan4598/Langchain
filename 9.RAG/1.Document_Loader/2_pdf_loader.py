998092d08a6b173cc1a818600b0d26d4554f38bd
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-06 12:06:40 +0530

    PyPDFLoader

--- 9.RAG/1.Document_Loader/2_pdf_loader.py
+++ 9.RAG/1.Document_Loader/2_pdf_loader.py
@@ -4,6 +4,9 @@ loader=PyPDFLoader(r"C:\Users\shrav\Desktop\Langchain code\9.RAG\1.Document_Load
 
 docs=loader.load()
 print(docs)
+print("======"*10)
 print(len(docs))
+print("======"*10)
 print(docs[0].page_content)
-print(docs[1].metadata)
+print("======"*10)
+print(docs[0].metadata)
