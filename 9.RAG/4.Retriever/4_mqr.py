d82cbcd293bc6bb3620eab217d73f5d5c48201dc
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-08 00:22:04 +0530

    Multi Query Retriever

---
+++ 9.RAG/4.Retriever/4_mqr.py
@@ -0,0 +1,90 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+model=ChatGoogleGenerativeAI(model="")
+
+all_docs = [
+    Document(page_content="Regular walking boosts heart health and can reduce symptoms of depression.", metadata={"source": "H1"}),
+    Document(page_content="Consuming leafy greens and fruits helps detox the body and improve longevity.", metadata={"source": "H2"}),
+    Document(page_content="Deep sleep is crucial for cellular repair and emotional regulation.", metadata={"source": "H3"}),
+    Document(page_content="Mindfulness and controlled breathing lower cortisol and improve mental clarity.", metadata={"source": "H4"}),
+    Document(page_content="Drinking sufficient water throughout the day helps maintain metabolism and energy.", metadata={"source": "H5"}),
+    Document(page_content="The solar energy system in modern homes helps balance electricity demand.", metadata={"source": "I1"}),
+    Document(page_content="Python balances readability with power, making it a popular system design language.", metadata={"source": "I2"}),
+    Document(page_content="Photosynthesis enables plants to produce energy by converting sunlight.", metadata={"source": "I3"}),
+    Document(page_content="The 2022 FIFA World Cup was held in Qatar and drew global energy and excitement.", metadata={"source": "I4"}),
+    Document(page_content="Black holes bend spacetime and store immense gravitational energy.", metadata={"source": "I5"}),
+]
+
+embedding_model=HuggingFaceEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
+
+vector_store=FAISS.from_documents(
+    documents=all_docs,
+    embedding=embedding_model
+)
+
+base_retriever=vector_store.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k":5}
+)
+
+multi_query_retriver=MultiQueryRetriever.from_llm(
+    retriver=vector_store.as_retriever(search_kwargs={"k":5}),
+    llm=ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+)
+
+query="How to improve energy levels and maintain balance?"
+
+similarity_results=base_retriever.invoke(query)
+multi_query_result=multi_query_retriver.invoke(query)
+
+for i, doc in enumerate(similarity_results):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
+
+print("*"*150)
+
+for i, doc in enumerate(multi_query_result):
+    print(f"\n--- Result {i+1} ---")
+    print(doc.page_content)
+
+#output
+"""
+
+--- Result 1 ---
+Drinking sufficient water throughout the day helps maintain metabolism and energy.
+
+--- Result 2 ---
+Mindfulness and controlled breathing lower cortisol and improve mental clarity.
+
+--- Result 3 ---
+Regular walking boosts heart health and can reduce symptoms of depression.
+
+--- Result 4 ---
+Deep sleep is crucial for cellular repair and emotional regulation.
+
+--- Result 5 ---
+The solar energy system in modern homes helps balance electricity demand.
+******************************************************************************************************************************************************
+
+--- Result 1 ---
+Drinking sufficient water throughout the day helps maintain metabolism and energy.
+
+--- Result 2 ---
+Mindfulness and controlled breathing lower cortisol and improve mental clarity.
+
+--- Result 3 ---
+Regular walking boosts heart health and can reduce symptoms of depression.
+
+--- Result 4 ---
+Consuming leafy greens and fruits helps detox the body and improve longevity.
+
+--- Result 5 ---
+Deep sleep is crucial for cellular repair and emotional regulation.
+"""
