97d6cf5c93a0c43518f6b1614b5c13dc51f9813e
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-10 13:13:40 +0530

    duckduckgo search

--- 10.Tools/1_duckduckgo_search.py
+++ 10.Tools/1_duckduckgo_search.py
@@ -2,6 +2,6 @@ from langchain_community.tools import DuckDuckGoSearchRun
 
 search_tool=DuckDuckGoSearchRun()
 
-results=search_tool.invoke("Who won ipl 2026")
+results=search_tool.invoke("Shravan Kumar Pandey linkedin")
 print(results)
 
