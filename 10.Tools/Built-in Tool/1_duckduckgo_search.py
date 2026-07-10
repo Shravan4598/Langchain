7e7aa61c45c27261e806bfe2aa8fc09c6400382d
Shravan4598 <shravankumarpandey825412@gmail.com>
2026-07-10 15:06:15 +0530

    strucured_tool

--- 10.Tools/Built-in Tool/1_duckduckgo_search.py
+++ 10.Tools/Built-in Tool/1_duckduckgo_search.py
@@ -5,3 +5,6 @@ search_tool=DuckDuckGoSearchRun()
 results=search_tool.invoke("Shravan Kumar Pandey linkedin")
 print(results)
 
+print("Description:",search_tool.description)
+print("Name:",search_tool.name)
+print("Arguments:",search_tool.args)
---
+++ 10.Tools/Custom_Tool/2_structured_tool.py
@@ -0,0 +1,37 @@
+from langchain_community.tools import StructuredTool
+from pydantic import Field,BaseModel
+
+class multiply_input(BaseModel):
+    a:int=Field(required=True,description="The first number to multiply")
+    b:int=Field(required=True,description="The second number to multiply")
+
+def multiply_func(a:int,b:int)->int:
+    return a*b
+
+multiply_tool=StructuredTool.from_function(
+    func=multiply_func,
+    name="Multiply",
+    description="Multiply two numbers",
+    args_schema=multiply_input
+)
+
+result=multiply_tool.invoke({"a":5,"b":3})
+print(result)
+print("===="*10)
+
+print("Name",multiply_tool.name)
+print("Description",multiply_tool.description)
+print("Arguments",multiply_tool.args)
+print(multiply_tool.args_schema)
+
+print(multiply_tool.args_schema.model_json_schema())
+
+"""
+15
+========================================
+Name Multiply
+Description Multiply two numbers
+Arguments {'a': {'description': 'The first number to multiply', 'title': 'A', 'type': 'integer'}, 'b': {'description': 'The second number to multiply', 'title': 'B', 'type': 'integer'}}
+<class '__main__.multiply_input'>
+{'properties': {'a': {'description': 'The first number to multiply', 'required': True, 'title': 'A', 'type': 'integer'}, 'b': {'description': 'The second number to multiply', 'required': True, 'title': 'B', 'type': 'integer'}}, 'required': ['a', 'b'], 'title': 'multiply_input', 'type': 'object'}
+"""
